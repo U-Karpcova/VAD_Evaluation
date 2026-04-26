c0a4e6143491b8567fc559a13a92b41174963234
Юліана <karpcova2004@gmail.com>
2026-04-27 02:02:51 +0300

    fixed dataset_analysis_app.py

--- analysis_app/dataset_analysis_app.py
+++ analysis_app/dataset_analysis_app.py
@@ -111,17 +111,17 @@ with gr.Blocks() as demo:
                         )
 
 
-        # initial load
-        demo.load(
-            fn=lambda: (
-                parser.plot_emotions("all", "count"),
-                parser.plot_regions("count"),
-                parser.plot_age("count"),
-                parser.plot_gender("count"),
-                parser.plot_emotions_by_gender("count"),
-                ),
-            outputs=[plot_emotions, region_plot, age_plot, gender_plot, gender_emotions_plot]
-        )
+                # initial load
+                demo.load(
+                    fn=lambda: (
+                        parser.plot_emotions("all", "count"),
+                        parser.plot_regions("count"),
+                        parser.plot_age("count"),
+                        parser.plot_gender("count"),
+                        parser.plot_emotions_by_gender("count"),
+                        ),
+                    outputs=[plot_emotions, region_plot, age_plot, gender_plot, gender_emotions_plot]
+                    )
 
 demo.launch()
 
